fd5b64146de2b5a9e12edd8a9fdf03a04ea8138b
Sovith kumar Singh <155513351+Sovith07@users.noreply.github.com>
2026-05-26 17:51:10 +0530

    Add files via upload

---
+++ Langchain prompts/chatbot.py
@@ -0,0 +1,27 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+import os
+import streamlit as st
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+
+os.environ['HF_HOME'] = 'D:/huggingface_cache'
+llm = HuggingFacePipeline.from_model_id(
+    model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
+    task='text-generation',
+   
+)
+model = ChatHuggingFace(llm=llm)
+
+chat_history= [
+    SystemMessage(content='You are a helpful AI assistant')]
+
+
+while True:
+    user_input=input('You: ')
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input=='exit':
+        break
+    result=model.invoke(user_input)
+    chat_history.append(AIMessage(content=result.content))
+    print('AI: ',result.content)
+
+#print(chat_history)
---
+++ Langchain prompts/prompt_template.py
@@ -0,0 +1,29 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+import os
+
+
+os.environ['HF_HOME'] = 'D:/huggingface_cache'
+llm = HuggingFacePipeline.from_model_id(
+    model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
+    task='text-generation',
+    pipeline_kwargs=dict(
+        temperature=0.5,
+        max_new_tokens=200
+    )
+)
+model = ChatHuggingFace(llm=llm)
+
+
+# detailed way
+template2 = PromptTemplate(
+    template='Greet this person in 5 languages. The name of the person is {name}',
+    input_variables=['name']
+)
+
+# fill the values of the placeholders
+prompt = template2.invoke({'name':'nitish'})
+
+result = model.invoke(prompt)
+
+print(result.content)
